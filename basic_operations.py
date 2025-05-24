b3cdef6f6a634e5cd5c2321a611e0a0ca2031486
Alisena Danishwer <danishweralisena@gmail.com>
2025-05-24 22:05:57 +0800

    The first exercise

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10 
+number2 = 5
+# The sum of two numbers
+print("Addition of 10 and 5 is " , number1 + number2)
+
+# The subtraction of two numbers
+
+print("Subtraction of 10 and 5 is " ,  number1 - number2)
+
+# The product of two numbers
+
+print("Multipllication of  10 and 5 is " , number1 * number2)
+
+
+
